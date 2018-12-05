70396ff10ae3d7d0c1f6420bcf0138ebd9b59178
Gregory <grgrsm@gmail.com>
2018-12-05 20:12:04 +0200

    revert

--- start.js
+++ start.js
@@ -17,7 +17,11 @@ app.use(require('express-session')({
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	secret: process.env.SESSION_SECRET,
+=======
+	secret: 'MwEwjtGQ3gzQBas8nsCrFwMmztdNERuSJQZpkwwZVLkrRfrNxg5WsffBcf9Psfzq',
+>>>>>>> parent of 44eab20... change to accept env variables
 =======
 	secret: 'MwEwjtGQ3gzQBas8nsCrFwMmztdNERuSJQZpkwwZVLkrRfrNxg5WsffBcf9Psfzq',
 >>>>>>> parent of 44eab20... change to accept env variables
